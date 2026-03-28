c95c1cd3525c0611dbad083a20862a0d5eac4729
Alfito07 <129337299+Alfito07@users.noreply.github.com>
2026-03-29 01:07:43 +0700

    Update templates.js
    Update template ADE 1.3

--- js/templates.js
+++ js/templates.js
@@ -5,7 +5,7 @@
 class TemplateManager {
   constructor() {
     this.templates = {
-      standard: `{salam} Kak {nama}, sebelumnya kami memohon maaf atas kendala yang dialami. Kami dari ICONNET ingin follow-up terkait laporan kendala {kategori} dengan No. Tiket pelaporan {tiket}.
+      standard: `{salam} Kak {nama}. Sebelumnya kami memohon maaf atas kendala yang dialami. Kami dari ICONNET ingin follow-up terkait laporan kendala {kategori} dengan No. Tiket pelaporan {tiket}.
 
 Sudah kami lakukan action dari sistem, silahkan dicoba kembali akses internetnya. Sampaikan pada kami apabila masih ada kendala?{asp}`,
 
